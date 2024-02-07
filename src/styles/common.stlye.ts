f0ca81d97fe34cec347177d21b0e85cd5d1af7b7
LeeDaeGyeong <dleorud1027@gmail.com>
2024-02-07 17:25:04 +0900

    Feat : styled-components 세팅

---
+++ src/styles/common.stlye.ts
@@ -0,0 +1,17 @@
+import { css } from "styled-components";
+
+export const FlexCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const AlignCenter = css`
+  display: flex;
+  align-items: center;
+`;
+
+export const JustifyCenter = css`
+  display: flex;
+  align-items: center;
+`;
