73e6a01c7ca25ce722b790a258d09c50c49d85a9
LeeDaeGyeong <dleorud1027@gmail.com>
2024-02-07 17:37:47 +0900

    Feat : react-router-dom 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme.style";
 import GlobalStyles from "./styles/global.style";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
 
-function App() {
+const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles></GlobalStyles>
+      <GlobalStyles>
+        <RouterProvider router={router} />
+      </GlobalStyles>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
