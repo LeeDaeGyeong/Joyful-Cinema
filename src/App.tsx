2414ca80ee880acc58898438e95d519bf63af47f
LeeDaeGyeong <dleorud1027@gmail.com>
2024-02-08 04:09:28 +0900

    Feat : RTk 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import theme from "./styles/theme.style";
 import GlobalStyles from "./styles/global.style";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles>
-        <RouterProvider router={router} />
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
       </GlobalStyles>
     </ThemeProvider>
   );
